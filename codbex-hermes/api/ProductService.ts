4d98a1bd0de59decb5ed35ad57285b0febd4527d
TIVMOF <tomyivanov06@gmail.com>
2024-05-31 06:12:21 +0000

    chore: Removing unnecessary views

--- codbex-hermes/api/ProductService.ts
+++ codbex-hermes/api/ProductService.ts
@@ -1,62 +1,56 @@
-import { ProductRepository as ProductDao } from "codbex-products/gen/dao/Products/ProductRepository";
-import { ProductCategoryRepository as CategoryDao } from "codbex-products/gen/dao/Categories/ProductCategoryRepository";
-import { SalesInvoiceItemRepository as SalesInvoiceItemDao } from "codbex-invoices/gen/dao/salesinvoice/SalesInvoiceItemRepository";
+// import { ProductRepository as ProductDao } from "codbex-products/gen/dao/Products/ProductRepository";
+// import { ProductCategoryRepository as CategoryDao } from "codbex-products/gen/dao/Categories/ProductCategoryRepository";
 
-import { Controller, Get } from "sdk/http";
-import { query } from "sdk/db";
-import { response } from "sdk/http";
+// import { Controller, Get } from "sdk/http";
+// import { query } from "sdk/db";
+// import { response } from "sdk/http";
 
-@Controller
-class ProductService {
+// @Controller
+// class ProductService {
 
-    private readonly productDao;
-    private readonly categoryDao;
-    private readonly salesInvoiceItemDao;
+//     private readonly productDao;
+//     private readonly categoryDao;
 
-    constructor() {
-        this.productDao = new ProductDao();
-        this.categoryDao = new CategoryDao();
-        this.salesInvoiceItemDao = new SalesInvoiceItemDao();
-    }
+//     constructor() {
+//         this.productDao = new ProductDao();
+//         this.categoryDao = new CategoryDao();
+//     }
 
-    @Get("/productData")
-    public productData() {
-        const currentDate = new Date();
-        currentDate.setHours(0, 0, 0, 0);
+//     @Get("/productData")
+//     public productData() {
+//         const currentDate = new Date();
+//         currentDate.setHours(0, 0, 0, 0);
 
-        const allProducts = this.productDao.findAll();
-        let activeProducts = this.productDao.findAll({
-            $filter: {
-                equals: {
-                    Enabled: true
-                }
-            }
-        }).length;
-        let inactiveProducts = this.productDao.findAll({
-            $filter: {
-                equals: {
-                    Enabled: false
-                }
-            }
-        }).length;
+//         const allProducts = this.productDao.findAll();
+//         let activeProducts = this.productDao.findAll({
+//             $filter: {
+//                 equals: {
+//                     Enabled: true
+//                 }
+//             }
+//         }).length;
+//         let inactiveProducts = this.productDao.findAll({
+//             $filter: {
+//                 equals: {
+//                     Enabled: false
+//                 }
+//             }
+//         }).length;
 
-        const activeCategories: number = this.categoryDao.count();
+//         const activeCategories: number = this.categoryDao.count();
 
-        const sql = "SELECT p.PRODUCT_NAME as NAME, COUNT(si.SALESINVOICEITEM_ID) AS ORDER_COUNT, SUM(si.SALESINVOICEITEM_GROSS) AS REVENUE_SUM FROM CODBEX_PRODUCT p JOIN CODBEX_SALESINVOICEITEM si ON p.PRODUCT_ID = si.SALESINVOICEITEM_PRODUCT GROUP BY p.PRODUCT_ID, p.PRODUCT_NAME ORDER BY order_count DESC LIMIT 5";
-        let resultset = query.execute(sql);
+//         const topProducts = resultset.map(row => ({
+//             productName: row.NAME,
+//             orderCount: row.ORDER_COUNT,
+//             revenue: row.REVENUE_SUM
+//         }));
 
-        const topProducts = resultset.map(row => ({
-            productName: row.NAME,
-            orderCount: row.ORDER_COUNT,
-            revenue: row.REVENUE_SUM
-        }));
-
-        return {
-            "ActiveProducts": activeProducts,
-            "InactiveProducts": inactiveProducts,
-            "AllProducts": allProducts.length,
-            "ActiveCategories": activeCategories,
-            "TopProducts": topProducts
-        };
-    }
-}
+//         return {
+//             "ActiveProducts": activeProducts,
+//             "InactiveProducts": inactiveProducts,
+//             "AllProducts": allProducts.length,
+//             "ActiveCategories": activeCategories,
+//             "TopProducts": topProducts
+//         };
+//     }
+// }
--- codbex-hermes/subviews/dashboard.js
+++ codbex-hermes/subviews/dashboard.js
@@ -51,26 +51,16 @@ dashboard.controller('DashboardController', ['$scope', '$document', '$http', 'me
             messageHub.postMessage('launchpad.switch.perspective', { perspectiveId: 'sales-orders' }, true);
         } else if (perspective === 'products') {
             messageHub.postMessage('launchpad.switch.perspective', { perspectiveId: 'products' }, true);
-        } else if (perspective === 'sales-invoices') {
-            messageHub.postMessage('launchpad.switch.perspective', { perspectiveId: 'sales-invoices' }, true);
         }
         ;
     }
 
     $scope.today = new Date();
 
-    const invoiceServiceUrl = "/services/ts/codbex-hermes/api/InvoiceService.ts/invoiceData";
-    $http.get(invoiceServiceUrl)
-        .then(function (response) {
-            $scope.InvoiceData = response.data;
-            calculateGrossProfit();
-        });
-
     const orderServiceUrl = "/services/ts/codbex-hermes/api/OrderService.ts/orderData";
     $http.get(orderServiceUrl)
         .then(function (response) {
             $scope.OrderData = response.data;
-            calculateGrossProfit();
         });
 
     const productServiceUrl = "/services/ts/codbex-hermes/api/ProductService.ts/productData";
@@ -85,12 +75,6 @@ dashboard.controller('DashboardController', ['$scope', '$document', '$http', 'me
             return response.data;
         } catch (error) {
             console.error('Error fetching product data:', error);
-        }
-    }
-
-    function calculateGrossProfit() {
-        if ($scope.InvoiceData && $scope.OrderData) {
-            $scope.GrossProfit = (($scope.InvoiceData.SalesInvoiceTotal + $scope.OrderData.SalesOrderTotal) - ($scope.InvoiceData.PurchaseInvoiceTotal + $scope.OrderData.PurchaseOrderTotal)).toFixed(2);
         }
     }
 
