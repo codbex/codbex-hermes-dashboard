e9297f2f6dda2cc670ebc797f7ac84b698557049
TIVMOF <tomyivanov06@gmail.com>
2024-07-03 14:49:46 +0000

    feat: initial numeric tile implementation

--- codbex-hermes/subviews/dashboard.js
+++ codbex-hermes/subviews/dashboard.js
@@ -1,4 +1,4 @@
-const dashboard = angular.module('dashboard', ['ideUI', 'ideView']);
+const dashboard = angular.module('dashboard', ['ideUI', 'ideView', 'applicationTiles']);
 
 dashboard.controller('DashboardController', ['$scope', '$document', '$http', 'messageHub', function ($scope, $document, $http, messageHub) {
 
