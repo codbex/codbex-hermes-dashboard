ed7e7c2ba8c1dac6248970046f5a9c00a7f99b72
TIVMOF <tomyivanov06@gmail.com>
2024-07-08 04:35:38 +0000

    chore: Testing tiles

--- codbex-hermes/subviews/dashboard.js
+++ codbex-hermes/subviews/dashboard.js
@@ -1,6 +1,28 @@
-const dashboard = angular.module('dashboard', ['ideUI', 'ideView', 'applicationTiles']);
+const dashboard = angular.module('dashboard', ['ideUI', 'ideView', "applicationTiles"]);
 
 dashboard.controller('DashboardController', ['$scope', '$document', '$http', 'messageHub', function ($scope, $document, $http, messageHub) {
+    $scope.state = {
+        isBusy: true,
+        error: false,
+        busyText: "Loading...",
+    };
 
+    angular.element($document[0]).ready(async function () {
+
+        $scope.$apply(function () {
+            $scope.state.isBusy = false;
+        });
+    });
+
+    $scope.today = new Date();
+
+    async function getProductData() {
+        try {
+            const response = await $http.get("/services/ts/codbex-athena/api/ProductService.ts/productData");
+            return response.data;
+        } catch (error) {
+            console.error('Error fetching product data:', error);
+        }
+    }
 
 }]);
