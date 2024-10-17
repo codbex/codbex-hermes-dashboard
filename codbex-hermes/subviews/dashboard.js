c63234c5e8ee69a30c3d97fbce884f462ca35e1a
TIVMOF <78173711+TIVMOF@users.noreply.github.com>
2024-10-17 16:33:56 +0300

    Navigation extensions update (#76)

--- codbex-hermes/subviews/dashboard.js
+++
@@ -1,6 +0,0 @@
-const dashboard = angular.module('dashboard', ['ideUI', 'ideView']);
-
-dashboard.controller('DashboardController', ['$scope', '$document', '$http', 'messageHub', function ($scope, $document, $http, messageHub) {
-
-
-}]);
